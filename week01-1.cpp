fd711a40d7efb517bb3e91a3ee44dd8ad1d009d4
322025 <wangpinyaow@gmail.com>
2025-02-21 12:15:23 +0800

    Add files via upload

---
+++ week01-1.cpp
@@ -0,0 +1,12 @@
+///week01-1.cpp要自己打好全部的檔名,後面要打.cpp
+///在瘋狂澄社(練習模式)寫好的程式碼,全選,Ctrl-c
+///CodeBlocks File-New-Empty空白檔案,Ctrl-s存檔
+#include <stdio.h>///使用standard標準的input/output
+
+int main()
+{
+	int a, b;///整數a,b
+	scanf("%d%d",&a,&b);///讓人讓人查資料(小心"#d#d"後面逗號
+	///兩個%d%d
+	printf("%d",a+b);///印出資料,把答案印出來
+}
