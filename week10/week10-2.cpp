8651b42d6b519fcae53851dd51dd264e1a3b52c2
322025 <wangpinyaow@gmail.com>
2025-04-25 11:55:23 +0800

    week10

---
+++ week10/week10-2.cpp
@@ -0,0 +1,15 @@
+///week10-2.cpp
+#include <stdio.h>
+int main()
+{
+    printf("叫块俱计:");
+    int n;
+    scanf("%d",&n);
+    ///printf("计琌 %d\n",n%10);
+    ///printf("计琌 %d\n",n/10 %10);
+    ///printf("κ计琌 %d\n",n/100 %10);
+    while(n>0){
+        printf("计%dブ:%d\n",n,n%10);
+        n=n/10;
+    }
+}
